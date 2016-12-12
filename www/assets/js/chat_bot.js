9bb67d69cdbbab2c8a88118dd6f4cdde41f26637
ParadoxCTRL <tom@pome.ro>
2016-12-11 22:14:31 -0500

    added real time graphs using CanvasJS

--- www/assets/js/chat_bot.js
+++ www/assets/js/chat_bot.js
@@ -4,7 +4,7 @@ var start_time = start_date.getTime();
 console.log(start_time);
 var options = {
     options: {
-        debug: true
+        debug: false
     },
     connection: {
         random: "chat",
@@ -12,21 +12,58 @@ var options = {
     },
     identity: {
         username: "Aw_kpmbot",
-        password: "PW"
+        password: "oauth:ljs4xxjzsxqop50kwq258t15khhbvz"
     },
     channels: ["#aw_kpmbot"]
 };
 
-var client = new irc.client(options);
+var client = new tmi.client(options);
 
 // Connect the client to the server..
 client.connect();
 
 var message_total = 0;
 var kappa_total = 0;
+var Pog_total = 0
 var kappa_ratio;
 var kpm = 0;
 var mpm = 0;
+var ppm = 0;
+
+
+var dataPointsKappa = [];
+dataPointsKappa.push({y: kpm, x: 0})
+var dataPointsMsgs = [];
+dataPointsKappa.push({y: mpm, x: 0})
+var dataPointsPog = [];
+dataPointsKappa.push({y: ppm, x: 0})
+
+window.onload = function(){
+    chart = new CanvasJS.Chart("Chart",{
+    title:{text:"Live Kappa Per Min"},
+    axisY:[
+      {title:"Kappa Per Min"},
+      {title:"Messages Per Min"},
+      {title:"PogChamps Per Min"}
+    ],
+    data:[{
+      type: "line",
+      axisYindex: 0,
+      dataPoints: dataPointsKappa
+    },{
+      type: "line",
+      axisYindex: 1,
+      dataPoints: dataPointsMsgs
+    },{
+      type: "line",
+      axisYindex: 2,
+      dataPoints: dataPointsPog
+    }]
+  })
+  chart.render();
+}
+
+var lastTime = start_time
 
 client.on("chat", function (channel, user, message, self) {
 
@@ -37,12 +74,33 @@ client.on("chat", function (channel, user, message, self) {
       cur_date = new Date();
       cur_time = cur_date.getTime();
       kpm = Math.floor((kappa_total / ((cur_time - start_time) / 1000 / 60 )));
+      ppm = Math.floor((Pog_total / ((cur_time - start_time) / 1000 / 60 )));
       mpm = Math.floor((message_total / ((cur_time - start_time) / 1000 / 60 )));
+      runningTime = Math.floor((cur_time / 1000) - (start_time / 1000))
+      // Push Data to Chart
+      dataPointsKappa.push({
+        y:kpm,
+        x:runningTime
+      })
+      dataPointsMsgs.push({
+        y:mpm,
+        x:runningTime
+      })
+      dataPointsPog.push({
+        y:ppm,
+        x:runningTime
+      })
+      timeBetweenRenders = (cur_time / 1000) - (lastTime / 1000)
+      if(timeBetweenRenders > 2){
+        lastTime = cur_time
+        chart.render()
+      }
 
       $("#kpm_display").text(kpm + " KPM");
       message_total += 1;
 
-      KappaRegex = /\b(Kappa|KappaPride)\b/g;
+      KappaRegex = /\b(Kappa|KappaPride|KappaHD|KappaRoss|KappaClaus|KappaWealth)\b/g;
+      PogRegex = /\b(PogChamp)\b/g;
       kpmRegex = /(!kpm)/;
       joinRegex = /(!join)/;
 
@@ -52,6 +110,16 @@ client.on("chat", function (channel, user, message, self) {
           kappa_total += 1;
         }
           $("#kappa_msg").text(message);
+      }
+
+      if (message.match(PogRegex)) {
+        PogMSG_count = message.match(PogRegex);
+        for (var i = 0; i < PogMSG_count.length; i++) {
+          Pog_total += 1;
+          console.log(message);
+          console.log(Pog_total)
+          console.log(ppm)
+        }
       }
 
       $("#message_count").text(message_total);
